1ee167484c0744d700e6ce96c9a3423cd2c77ebb
dgnsrekt <run2devtest@gmail.com>
2019-04-17 23:06:31 -0500

    preparing classes

--- baseball_simulator.py
+++ baseball_simulator.py
@@ -12,6 +12,7 @@ Goals:
 simulated games.
 """
 import random
+from collections import deque
 
 
 def dice():
@@ -24,3 +25,64 @@ def dice():
     """
     roll = (random.randint(1, 6), random.randint(1, 6))
     return sorted(roll)
+
+
+class Runner:
+    """
+    Description:
+    Arguments:
+        None
+    Returns:
+    """
+
+    def __init__(self):
+        self.current_base = 0
+
+    def run(self):
+        pass
+
+    def has_scored(self):
+        pass
+
+
+class BaseballSimulator:
+    """
+    Description:
+    Arguments:
+        None
+    Returns:
+    """
+
+    def __init__(self):
+        self.field = deque()
+        self.innings = 0
+        self.strikes = 0
+        self.outs = 0
+        self.plays = 0
+
+    def map_dice_action(self, dice):
+        """
+        Description:
+        Arguments:
+            None
+        Returns:
+        """
+        pass
+
+    def step(self, action):
+        """
+        Description:
+        Arguments:
+            None
+        Returns:
+        """
+        pass
+
+    def done(self):
+        """
+        Description:
+        Arguments:
+            None
+        Returns:
+        """
+        pass
