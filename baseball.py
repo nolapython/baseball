6e0cab314fd4c3b35c8d5987af92113d26edfd33
dgnsrekt <run2devtest@gmail.com>
2019-04-17 21:49:28 -0500

    added dice function.

--- baseball.py
+++
@@ -1,2 +0,0 @@
-def dice():
-    pass
---
+++ baseball_simulator.py
@@ -0,0 +1,26 @@
+"""
+Title: baseball-simulator
+Author: Kevin Tracey McGee (DGNSREKT)
+Date: 4/17/2019
+Description: Baseball + Yahtzee: A baseball simulator which uses
+two standard dice to simulate the outcomes of a baseball game.
+Url: https://github.com/nolapython/baseball
+
+Goals:
+1. Find the average number of runs that would be scored in 9 innings.
+2. Find the distribution of the number of runs scored across multiple
+simulated games.
+"""
+import random
+
+
+def dice():
+    """
+    Description: Simulate rolling 2 standerd 6 sided dice.
+    Arguments:
+        None
+    Returns:
+        Result of the dice roll. -- tuple(int, int)
+    """
+    roll = (random.randint(1, 6), random.randint(1, 6))
+    return sorted(roll)
